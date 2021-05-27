5889639fb464ffd4388f64949c70b83a9dd5bd6d
Tepirek <tepirek@outlook.com>
2021-05-27 20:44:07 +0200

    refactored encryption, started decryption file

--- src/main/java/pl/edu/pg/student/cybersecurity/App.java
+++ src/main/java/pl/edu/pg/student/cybersecurity/App.java
@@ -1,27 +1,22 @@
 package pl.edu.pg.student.cybersecurity;
 
-import pl.edu.pg.student.cybersecurity.Layouts.Login;
-import pl.edu.pg.student.cybersecurity.Layouts.Register;
-import pl.edu.pg.student.cybersecurity.Layouts.Welcome;
-import pl.edu.pg.student.cybersecurity.System.Api;
-import pl.edu.pg.student.cybersecurity.System.Encryptor;
+import pl.edu.pg.student.cybersecurity.System.Decryptor;
 import pl.edu.pg.student.cybersecurity.System.KeyHandler;
-import pl.edu.pg.student.cybersecurity.System.User;
 
 import java.io.File;
+import java.security.Key;
 
 public class App {
 
     public static void main(String[] args) {
 
         // Welcome welcome = new Welcome();
+        Integer size = 512;
+        File file = new File("./testing/test2_encrypted.txt");
+        KeyHandler keyHandler = new KeyHandler(size);
 
-        KeyHandler keyHandler = new KeyHandler(512);
-        File file = new File("test2.txt");
-
-        Encryptor encryptor = new Encryptor(512, keyHandler.getPublicKey(), file);
-        encryptor.encrypt("OnlyRSA");
-
+        Decryptor decryptor = new Decryptor(keyHandler.getPrivateKey(), file);
+        decryptor.decrypt();
     }
     
 }
--- src/main/java/pl/edu/pg/student/cybersecurity/System/Decryptor.java
+++ src/main/java/pl/edu/pg/student/cybersecurity/System/Decryptor.java
@@ -1,6 +1,9 @@
 package pl.edu.pg.student.cybersecurity.System;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.PrivateKey;
 import java.util.List;
 
@@ -9,11 +12,24 @@ public class Decryptor {
     private PrivateKey privateKey;
     private File file;
 
-    public void decrypt() {
-
+    public Decryptor(PrivateKey privateKey, File file) {
+        this.privateKey = privateKey;
+        this.file = file;
     }
 
-    private byte[] handleMetadata() {
+    public void decrypt() {
+        readMetadata();
+    }
+
+    private List<Object> readMetadata() {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] metadata = new byte[4];
+            fileInputStream.read(metadata, 0, 4);
+            Integer size = ByteBuffer.wrap(metadata).getInt();
+            System.out.println("Key size = " + size);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
